67ef6d9336de2f13e884e418c0652595e33adb90
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-22 18:30:23 -0500

    Creacion del InterfaceProductService

---
+++ backend/src/main/java/com/sc/backend/interfaceService/InterfaceProductService.java
@@ -0,0 +1,18 @@
+package com.sc.backend.interfaceService;
+
+import java.util.List;
+
+import com.sc.backend.entity.Product;
+
+public interface InterfaceProductService {
+	public List<Product> listProductId(Long userId);
+	public Product listOneProductId(Long productId);
+	public Product addProduct(Product product, Long userId);
+	public Product changeState(Product product);
+	public Product updateBalance(Product product, Long userId);
+	public List<Product> listIdOtherAvailableProducts(Long userId, Long productId);
+	public Product cancelProduct(Product product);
+	public Product addToBalance(Product product, int movement);
+	public Product withdrawToBalance(Product product, int movement);
+	
+}
