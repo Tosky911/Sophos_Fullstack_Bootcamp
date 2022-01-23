581c427e0e6dbe18bb43a678c6749c2c3ab1418b
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 17:38:05 -0500

    Configuracion funciones para el component add user

--- frontend/frontend/src/app/components/user/add-user/add-user.component.ts
+++ frontend/frontend/src/app/components/user/add-user/add-user.component.ts
@@ -1,4 +1,8 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/entities/user';
+import { UserService } from 'src/app/services/services/user/user.service';
 
 @Component({
   selector: 'app-add-user',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private userService:UserService) { }
+
+  dateNow = new Date();
 
   ngOnInit(): void {
+  }
+
+  user: User = {
+
+    //Tipo documento
+    typeId: '',
+    //Numero documento
+    numId: '',
+    //Nombre de usuario
+    username: '',
+    //Nombres
+    firsName: '',
+    //Apellidos
+    lastName: '',
+    //Correo
+    email: '',
+    //Fecha nacimiento
+    birthdayDate: '',
+    //Fecha creacion cuenta
+    creationDate: '',
+
+  };
+
+  //Guardar usuario
+  saveUser(): void{
+    const data={
+      typeId: this.user.typeId,
+      numId: this.user.numId,
+      username: this.user.username,
+      firstName: this.user.firsName,
+      lastName: this.user.lastName,
+      email: this.user.email,
+      birthdayDate: this.user.birthdayDate,
+      creationDate: formatDate(this.dateNow, 'YYYY-MM-dd', 'en-US')
+    };
+
+    this.userService.createUser(data)
+    .subscribe({
+      next: ()=>{
+      alert("El usuario ha sido creado exitosamete");
+      this.router.navigate(["users"])
+    },
+
+    error: (e) => console.error(e)
+    })
+
   }
 
 }
