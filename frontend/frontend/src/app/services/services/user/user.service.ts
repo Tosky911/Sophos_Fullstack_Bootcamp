fcd618c3193be15a508ff7944a16603c1a08fb81
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 16:29:30 -0500

    Creacion del user service

--- frontend/frontend/src/app/services/services/user/user.service.ts
+++ frontend/frontend/src/app/services/services/user/user.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+//import { User } from ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  UrlUser = 'http://localhost:8080/users/';
+  constructor(private http:HttpClient) { }
+
+  //Listar todos los clientes de la BBDD
+  getUser(): Observable<User[]>{
+    return this.http.get<User[]>(this.UrlUser);
+  }
+  
+  //Crear un usuario
+  createUser(data: any): Observable<any>{
+    return this.http.post(this.UrlUser,data);
+  }
+
+  //Listar un solo usuario
+  getUserId(id:any): Observable<any>{
+    const url = `${this.UrlUser}${id}`;
+    return this.http.get(url);
+  }
+
+  //Actualizar los datos del usuario
+  updateUser(id:any, data:any): Observable<any>{
+    const url = `${this.UrlUser}${id}`;
+    return this.http.put(url, data);
+  }
+
+  //Borrar un usuario
+  deleteUser(id:any): Observable<any>{
+    const url = `${this.UrlUser}${id}`;
+    return this.http.delete(url);
+  }
+
 }
