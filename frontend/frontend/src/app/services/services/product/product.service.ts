ff0330fe71a4ff01c13308f325dd5915687993f1
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 16:07:03 -0500

    creacion del product service

--- frontend/frontend/src/app/services/services/product/product.service.ts
+++ frontend/frontend/src/app/services/services/product/product.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'; 
+//import {Product} from ;
+//import {User} from ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  UrlProduct = 'http://localhost:8080/users/';
+
+  constructor(private http: HttpClient) { }
+
+  //Listar todos los productos de un usuario
+  getProduct(id: any): Observable<Product[]> {
+    const url = `${this.UrlProduct}${id}/products`;
+    return this.http.get<Product[]>(url);
+  }
+
+  //Listar un producto por id
+  getOneProduct(id: any, productId :any): Observable<Product>{
+    const url = `${this.UrlProduct}${id}/products/${productId}`;
+    return this.http.get<Product>(url);
+  }
+
+  //Crear un producto para un usuario
+  createProduct(data: any, id: any): Observable<any>{
+    const url = `${this.UrlProduct}${id}/products`;
+    return this.http.post(url, data);
+  }
+
+  //Cambiar el state a activo o inactivo
+  updateStateProduct(id: any, productId: any, data:any): Observable<any>{
+    const url = `${this.UrlProduct}${id}/products/${productId}/changeState`;
+    return this.http.put(url, data);
+  }
+
+  //Cambiar el state cancelado
+  cancelStateProduct(id: any, productId: any, data:any): Observable<any>{
+    const url = `${this.UrlProduct}${id}/products/${productId}/cancel`;
+    return this.http.put(url, data);
+  }
+
+  //Agregar dinero a un producto/cuenta
+  addMoney(id: any, productId: any, money: any, data: any): Observable<any>{
+    const url = `${this.UrlProduct}${id}/products/${productId}/${money}/deposit`;
+    return this.http.put(url, data);
+  }
+
+  //Retirar dinero de un producto/cuenta
+  withdrawMoney(id: any, productId: any, money: any, data: any): Observable<any>{
+    const url = `${this.UrlProduct}${id}/products/${productId}/${money}/withdraw`;
+    return this.http.put(url, data);
+  }
+
 }
