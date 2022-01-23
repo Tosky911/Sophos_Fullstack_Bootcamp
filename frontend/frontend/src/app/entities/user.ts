76a8d99876cd215fa8b7ea31bce6b81ede64f6c2
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 16:45:58 -0500

    Creacion de la entidad user

---
+++ frontend/frontend/src/app/entities/user.ts
@@ -0,0 +1,19 @@
+export interface User{
+    id?: number;
+	//Tipo documento
+	typeId?: String;
+	//Numero documento
+	numId?: String;
+	//Nombre de usuario
+	username?: String;
+	//Nombres
+	firsName?: String;
+	//Apellidos
+	lastName?: String;
+	//Correo
+	email?: String;
+	//Fecha nacimiento
+	birthdayDate?: String;
+	//Fecha creacion cuenta
+	creationDate?: String;
+}
