d2dcb25518eb40b7aa0d7e0dadea0c1044fe3dcd
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 16:20:12 -0500

    Creacion del transaction service

--- frontend/frontend/src/app/services/services/transaction/transaction.service.ts
+++ frontend/frontend/src/app/services/services/transaction/transaction.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'; 
+//import { Product } from ;
+//import { Transaction } from ;
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
+  UrlTransaction = 'http://localhost:8080/users/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  //Listar todas las transacciones por producto/cuenta
+  getTransaction(id: any, productId: any): Observable<Transaction[]>{
+    const url = `${this.UrlTransaction}${id}/products/${productId}/transaction`;
+    return this.http.get<Transaction[]>(url);
+  }
+  
+  //Crear transaccion para un producto/cuenta
+  createTransaction(data: any, id:any, productId: any): Observable<any>{
+    const url = `${this.UrlTransaction}${id}/products/${productId}/transaction/create`;
+    return this.http.post(url, data);
+  }
+
+  //Listar todos los productos que pueden hacer transferencias de dinero
+  getProduct(id:any, productId: any): Observable<Product[]>{
+    const url = `${this.UrlTransaction}${id}/products/${productId}/transferable`;
+    return this.http.get<Product[]>(url);
+  }
+
+
 }
