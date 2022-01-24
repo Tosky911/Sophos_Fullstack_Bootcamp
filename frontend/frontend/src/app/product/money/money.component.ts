451ff8fdd8c7c602965b0b0974fd127a006bba2f
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 23:10:59 -0500

    Actualizacion del componente product/money

--- frontend/frontend/src/app/product/money/money.component.ts
+++ frontend/frontend/src/app/product/money/money.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/entities/product';
+import { ProductService } from 'src/app/services/services/product/product.service';
 
 @Component({
   selector: 'app-money',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoneyComponent implements OnInit {
 
-  constructor() { }
+  currentProduct: Product = {
+    typeAccount: '',
+    numAccount: '',
+    creationDate: '',
+    state: '',
+    balance: 0,
+  };
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productService: ProductService) { }
 
   ngOnInit(): void {
+
+  }
+
+  backProduct(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.router.navigate(['users', params.get('id'), 'products']);
+    });
+
+  }
+
+  addMoneytoProduct(userId: any, productId: any, money: any): void {    
+    this.route.paramMap.subscribe((params) => {
+      this.productService.addMoney(params.get('id'),params.get('productId'), this.currentProduct.balance, this.currentProduct)
+      .subscribe({
+        next: () => {
+          alert('Consignación realizada');
+          this.router.navigate(['users', params.get('id'), 'products']);
+        },
+        error: (e) => console.error(e),
+      });
+    });
+  }
+
+  withdrawMoneytoProduct(userId: any, productId: any, money: any): void {  
+    this.route.paramMap.subscribe((params) => {
+      this.productService.withdrawMoney(params.get('id'),params.get('productId'), this.currentProduct.balance, this.currentProduct)
+      .subscribe({
+        next: () => {
+          alert('Retiro realizado');
+          this.router.navigate(['clients', params.get('id'), 'products']);
+        },
+        error: (e) => console.error(e),
+      });
+    });
   }
 
 }
