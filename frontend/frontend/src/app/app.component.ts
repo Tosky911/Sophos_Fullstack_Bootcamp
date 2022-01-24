6f27ced56b334569d0999d45127cabbd2d4c7d21
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 21:45:10 -0500

    Actualizacion del app.component.ts

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'BankApp';
+
+  constructor(private router:Router){}
+
+  ngOnInit(): void {
+    this.router.navigate(['']);
+  }
+
+  List(){
+    this.router.navigate(['users']);
+  }
+
+  New(){
+    this.router.navigate(['addUser']);
+  }
+
 }
