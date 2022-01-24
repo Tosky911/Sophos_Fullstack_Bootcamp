9d145312c9cab019ad8fd64f358428fa77fd69cb
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 21:21:58 -0500

    Creacion component delete-user y BS con AngularWay

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AddUserComponent } from './components/user/add-user/add-user.component';
 
+import { AppComponent } from './app.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { UserService } from './services/services/user/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DeleteUserComponent } from './user/delete-user/delete-user.component';
 @NgModule({
   declarations: [
     AppComponent,
     AddUserComponent,
+    DeleteUserComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+function routes(routes: any, arg1: { onSameUrlNavigation: "reload"; }): any[] | import("@angular/core").Type<any> | import("@angular/core").ModuleWithProviders<{}> {
+  throw new Error('Function not implemented.');
+}
