99510cacca5547ca18e4ad79d6b5812f6e1d15b5
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 22:59:53 -0500

    Actualizacion componente add-product

--- frontend/frontend/src/app/product/add-product/add-product.component.ts
+++ frontend/frontend/src/app/product/add-product/add-product.component.ts
@@ -1,4 +1,9 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/entities/product';
+import { User } from 'src/app/entities/user';
+import { ProductService } from 'src/app/services/services/product/product.service';
 
 @Component({
   selector: 'app-add-product',
@@ -6,10 +11,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+  
+  dateNow = new Date();
 
-  constructor() { }
+  products?: Product[];
+  users?: User[];
 
-  ngOnInit(): void {
+  product: Product = {
+    typeAccount: '',
+    numAccount: '',
+    creationDate: '',
+    state: '',
+    balance: 0,
+  };
+  save = false;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {}
+
+  saveProduct(): void {
+    const data = {
+      typeAccount: this.product.typeAccount,
+      numAccount: this.product.numAccount,
+      state: 'activa',
+      creationDate: formatDate(this.dateNow, 'YYYY-MM-dd', 'en-US'),
+    };
+
+    this.route.paramMap.subscribe((params) => {
+      if (params.has('id')) {
+        this.productService
+          .getProduct(params.get('id'))
+          .subscribe((data) => (this.products = data));
+        this.productService.createProduct(data, params.get('id')).subscribe({
+          next: () => {
+            alert('El producto ha sido creado con éxito');
+            this.router.navigate(['users', params.get('id'), 'products']);
+          },
+          error: (e) => console.error(e),
+        });
+      }
+    });
   }
 
+  backProduct(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.router.navigate(['users', params.get('id'), 'products']);
+    });
+
+  }
 }
