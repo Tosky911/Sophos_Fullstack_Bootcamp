7771fc7f69da9f5d875bbbba129ac91f901e187d
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 23:48:19 -0500

    Creacion de todas las rutas dentro del app-routing

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './product/add-product/add-product.component';
+import { MoneyComponent } from './product/money/money.component';
+import { CreateTransactionComponent } from './transaction/create-transaction/create-transaction.component';
+import { ListTransactionComponent } from './transaction/list-transaction/list-transaction.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { DeleteUserComponent } from './user/delete-user/delete-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LandingComponent},
+  // To list all the clients
+  {path:'users', component:ListUserComponent},
+  // To create a client
+  {path:'addUser', component:AddUserComponent},
+  // To edit a client info
+  {path:'users/:id/products', component:EditUserComponent},
+  //To delete a client
+  {path:'users/:id/products', component:DeleteUserComponent},
+  //To create a product
+  {path:'users/:id/products/add', component:AddProductComponent},
+  //To make movement of money
+  {path:'users/:id/products/:productId/money', component:MoneyComponent},
+  //To list all the transaction of a product
+  {path:'users/:id/products/:productId/transactions', component:ListTransactionComponent},
+  //To create a transaction
+  {path:'users/:id/products/:productId/transaction', component:CreateTransactionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
