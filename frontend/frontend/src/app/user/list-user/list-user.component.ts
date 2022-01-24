370f8f27c30ea679997f1af7c36f6f56326e4326
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-23 22:29:48 -0500

    Actualizacion componente list-user

--- frontend/frontend/src/app/user/list-user/list-user.component.ts
+++ frontend/frontend/src/app/user/list-user/list-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/entities/user';
+import { UserService } from 'src/app/services/services/user/user.service';
 
 @Component({
   selector: 'app-list-user',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListUserComponent implements OnInit {
 
-  constructor() { }
+  users?:User[];
+  currentUser: User ={};
+  currentIndex = -1;
+
+  constructor(private service:UserService, private router:Router) { }
 
   ngOnInit(): void {
+    this.service.getUser().subscribe(data=>{this.users=data;})
+  }
+
+  setActiveUser(user: User): void{
+    this.currentUser = user;
+  }
+
+  EditUser(userId: number | undefined): void{
+    this.router.navigate(["users/",userId,"products"]);
   }
 
 }
