2b9a061d2d1cf072ca9471cad6f8251a76fd20ef
John Santiago Carmona Mesa <Santiagocarmona911@gmail.com>
2022-01-25 18:21:09 -0500

    Actualizacion del TransactionService y su Implementacion

--- backend/src/main/java/com/sc/backend/service/TransactionService.java
+++ backend/src/main/java/com/sc/backend/service/TransactionService.java
@@ -8,21 +8,13 @@ import com.sc.backend.entity.TransactionEntity;
 import com.sc.backend.repository.TransactionRepository;
 import com.sc.backend.service.impl.TransactionServiceImpl;
 
-@Service
-public class TransactionService implements TransactionServiceImpl{
 
-	@Autowired
-	private TransactionRepository transactionRepository;
+public interface TransactionService{
+
+	public TransactionEntity createTransaction(TransactionEntity transactionEntity, Long principalProductId);
+	public List<TransactionEntity> listTransactionId(Long principalProductId);
 	
-	@Override
-	public TransactionEntity createTransaction(TransactionEntity transactionEntity, Long principalProductId) {
-		return transactionRepository.save(transactionEntity);
-	}
-	
-	@Override
-	public List<TransactionEntity> listTransactionId(Long principalProductId) {
-		return transactionRepository.findByPrincipalProductIdAndTransactionResult(principalProductId, "Efectiva");
-	}
+
 	
 	
 }
--- backend/src/main/java/com/sc/backend/service/impl/TransactionServiceImpl.java
+++ backend/src/main/java/com/sc/backend/service/impl/TransactionServiceImpl.java
@@ -2,9 +2,26 @@ package com.sc.backend.service.impl;
 
 import java.util.List;
 
-import com.sc.backend.entity.TransactionEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface TransactionServiceImpl {
-	public TransactionEntity createTransaction(TransactionEntity transactionEntity, Long principalProductId);
-	public List<TransactionEntity> listTransactionId(Long principalProductId);
+import com.sc.backend.entity.TransactionEntity;
+import com.sc.backend.repository.TransactionRepository;
+import com.sc.backend.service.TransactionService;
+
+@Service
+public class TransactionServiceImpl implements TransactionService{
+	
+	@Autowired
+	private TransactionRepository transactionRepository;
+	
+	@Override
+	public TransactionEntity createTransaction(TransactionEntity transactionEntity, Long principalProductId) {
+		return transactionRepository.save(transactionEntity);
+	}
+	
+	@Override
+	public List<TransactionEntity> listTransactionId(Long principalProductId) {
+		return transactionRepository.findByPrincipalProductIdAndTransactionResult(principalProductId, "Efectiva");
+	}
 }
