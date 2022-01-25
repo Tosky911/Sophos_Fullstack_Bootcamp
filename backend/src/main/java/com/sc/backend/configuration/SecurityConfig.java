5f2a72cd985cd63802049497e1f78e5b964f6d60
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-24 21:42:06 -0500

    Creacion de la clase Security Config

---
+++ backend/src/main/java/com/sc/backend/configuration/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.sc.backend.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.sc.backend.service.impl.CustomerServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private CustomerServiceImpl customerServiceImpl;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
+	
+	@Autowired
+	private JwtFilters jwtFilters;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.customerServiceImpl(customerServiceImpl).passwordEncoder(bcrypt);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().antMatchers("/customers/auth")
+		.permitAll().anyRequest().authenticated()
+		.and().exceptionHandling().and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(jwtFilters, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManagerBean();
+	}
+	
+	
+}
