7e9005400b560179185da9d2696dce5839be6822
Santiago Carmona <90780637+Tosky911@users.noreply.github.com>
2022-01-24 21:47:24 -0500

    Actualización de JwtFilters

--- backend/src/main/java/com/sc/backend/configuration/JwtFilters.java
+++ backend/src/main/java/com/sc/backend/configuration/JwtFilters.java
@@ -1,5 +1,38 @@
 package com.sc.backend.configuration;
 
 public class JwtFilters {
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+	
+	@Autowired
+	private UserServiceImpl userDetailService;
 
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Headers", "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+		response.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+		response.setHeader("Allow","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+		
+		String token = request.getHeader("Authorization");
+		String userName = null;
+		if (token != null) {
+			userName = jwtUtils.extractUserName(token);
+		}
+		if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = userDetailService.loadUserByUsername(userName);
+			if (jwtUtils.validateToken(token, userDetails)) {
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+				
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+  
+  
 }
