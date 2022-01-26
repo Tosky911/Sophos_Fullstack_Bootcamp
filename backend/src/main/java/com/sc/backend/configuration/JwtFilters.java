867311a3a64bd356f77e83211ba75ce231d37651
Santiago Carmona <santiagocarmona911@gmail.com>
2022-01-26 14:31:06 -0500

    Desactivacion del Spring Security con comentarios

--- backend/src/main/java/com/sc/backend/configuration/JwtFilters.java
+++ backend/src/main/java/com/sc/backend/configuration/JwtFilters.java
@@ -1,57 +1,57 @@
-package com.sc.backend.configuration;
-
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.sc.backend.service.impl.UserServiceImpl;
-import com.sc.backend.util.JwtUtils;
-
-@Component
-public class JwtFilters extends OncePerRequestFilter {
-	
-	@Autowired
-	private JwtUtils jwtUtils;
-	
-	@Autowired
-	private UserServiceImpl userServiceImpl;
-
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Headers", "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-		response.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-		response.setHeader("Allow","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-		
-		String token = request.getHeader("Authorization");
-		String userName = null;
-		if (token != null) {
-			userName = jwtUtils.extractUserName(token);
-		}
-		if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = userServiceImpl.loadUserByUsername(userName);
-			if (jwtUtils.validateToken(token, userDetails)) {
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-				usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-				
-			}
-		}
-		filterChain.doFilter(request, response);
-	}
-  
-  
-}
+//package com.sc.backend.configuration;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import com.sc.backend.service.impl.UserServiceImpl;
+//import com.sc.backend.util.JwtUtils;
+//
+//@Component
+//public class JwtFilters extends OncePerRequestFilter {
+//	
+//	@Autowired
+//	private JwtUtils jwtUtils;
+//	
+//	@Autowired
+//	private UserServiceImpl userServiceImpl;
+//
+//	@Override
+//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//			throws ServletException, IOException {
+//		response.setHeader("Access-Control-Allow-Origin", "*");
+//		response.setHeader("Access-Control-Allow-Credentials", "true");
+//		response.setHeader("Access-Control-Allow-Headers", "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+//		response.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+//		response.setHeader("Allow","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+//		
+//		String token = request.getHeader("Authorization");
+//		String userName = null;
+//		if (token != null) {
+//			userName = jwtUtils.extractUserName(token);
+//		}
+//		if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//			UserDetails userDetails = userServiceImpl.loadUserByUsername(userName);
+//			if (jwtUtils.validateToken(token, userDetails)) {
+//				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//				usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+//				
+//			}
+//		}
+//		filterChain.doFilter(request, response);
+//	}
+//  
+//  
+//}
--- backend/src/main/java/com/sc/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/com/sc/backend/configuration/SecurityConfig.java
@@ -1,62 +1,62 @@
-package com.sc.backend.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.sc.backend.service.impl.UserServiceImpl;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	/*Debe llamarse userDetailsService para que el 
-	AuthenticationManagerBuilder funcione */
-	@Autowired
-	private UserServiceImpl userDetailsService;
-	
-	@Autowired
-	private BCryptPasswordEncoder bcrypt;
-	
-	@Autowired
-	private JwtFilters jwtFilters;
-	
-	/* Se agrega static para evitar un problema de referencia circular, 
-	se recomienda usar un setting injection */
-	@Bean
-	public static BCryptPasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		return bCryptPasswordEncoder;
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(bcrypt);
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/users/auth")
-		.permitAll().anyRequest().authenticated()
-		.and().exceptionHandling().and().sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
-		http.addFilterBefore(jwtFilters, UsernamePasswordAuthenticationFilter.class);
-	}
-	
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception{
-		return super.authenticationManagerBean();
-	}
-	
-	
-}
+//package com.sc.backend.configuration;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import com.sc.backend.service.impl.UserServiceImpl;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//	
+//	/*Debe llamarse userDetailsService para que el 
+//	AuthenticationManagerBuilder funcione */
+//	@Autowired
+//	private UserServiceImpl userDetailsService;
+//	
+//	@Autowired
+//	private BCryptPasswordEncoder bcrypt;
+//	
+//	@Autowired
+//	private JwtFilters jwtFilters;
+//	
+//	/* Se agrega static para evitar un problema de referencia circular, 
+//	se recomienda usar un setting injection */
+//	@Bean
+//	public static BCryptPasswordEncoder passwordEncoder() {
+//		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//		return bCryptPasswordEncoder;
+//	}
+//	
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.userDetailsService(userDetailsService).passwordEncoder(bcrypt);
+//	}
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.csrf().disable().authorizeRequests().antMatchers("/users/auth")
+//		.permitAll().anyRequest().authenticated()
+//		.and().exceptionHandling().and().sessionManagement()
+//		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//		
+//		http.addFilterBefore(jwtFilters, UsernamePasswordAuthenticationFilter.class);
+//	}
+//	
+//	@Bean
+//	@Override
+//	public AuthenticationManager authenticationManagerBean() throws Exception{
+//		return super.authenticationManagerBean();
+//	}
+//	
+//	
+//}
